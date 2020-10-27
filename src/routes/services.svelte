43f262bb1919f7ca41b0a6017e44ebc46c998b19
joxandev <joseinyork+dev@gmail.com>
2020-10-27 17:25:26 +0000

    Update

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -1,11 +1,9 @@
 <script>
   import Jumbo from "../components/Jumbo.svelte";
 </script>
-
 <svelte:head>
   <title>Services</title>
 </svelte:head>
-
 <Jumbo
   heading="Cirrus HR Services"
   subHeading="This is how we can help"
